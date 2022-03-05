993138c71ecb10c7673fdd518084fd04b79a024e
Laxmi Dutta <duttasuravi225@gmail.com>
2022-03-06 02:19:20 +0530

    ui of some pages are done add to cart page is connected with navbar

--- componants/navbar.js
+++ componants/navbar.js
@@ -2,15 +2,15 @@
 
     function navbar(){
         return `      <div id="search_pop_up">
-        <div class="cancle">cancle</div>
+        <div class="cancle">x</div>
         <p><b>WHAT ARE YOU LOOKING FOR?</b></p>
         <input type="text" id="search_input" placeholder="search product" />
       </div>
 
       <div id="navbar">
-      <a href = "../subscribeNow/subscribeNow.html">
-        <div class="subscribe"><h3>SUBSCRIBE NOW</h3></div>
-      </a>
+    
+        <div class="subscribe">  <a href = "../subscribeNow/subscribeNow.html"><h3>SUBSCRIBE NOW</h3> </a></div>
+     
         <div class="logo">
           <img
             src="https://cdn.shopify.com/s/files/1/0052/7551/6995/files/FABBAG-LOGO_96ac9306-4c81-4ab8-8d2b-4e818eed7f76_2048x.png?v=1606482289"
